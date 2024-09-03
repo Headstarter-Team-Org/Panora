2a05d67cb48088f1a3b78da8d6cf40ea9fb5e36a
maishaSupritee <maisha.sup@gmail.com>
2024-09-02 22:57:33 -0700

    🚧 Setup initial mappers.ts for wave financial companyinfo object

---
+++ packages/api/src/accounting/companyinfo/services/wave_financial/mappers.ts
@@ -0,0 +1,31 @@
+import {
+  UnifiedAccountingCompanyinfoInput,
+  UnifiedAccountingCompanyinfoOutput,
+} from '@accounting/companyinfo/types/model.unified';
+import { WaveFinancialCompanyInput, WaveFinancialCompanyOutput } from './types';
+import { ICompanyInfoMapper } from '@accounting/companyinfo/types';
+
+export class WaveFinancialCompanyMapper implements ICompanyInfoMapper {
+  desunify(
+    source: UnifiedAccountingCompanyinfoInput,
+    customFieldMappings?: {
+      slug: string;
+      remote_id: string;
+    }[],
+  ): WaveFinancialCompanyInput {
+    return Promise.resolve({});
+  }
+
+  unify(
+    source: any,
+    connectionId: string,
+    customFieldMappings?: {
+      slug: string;
+      remote_id: string;
+    }[],
+  ): Promise<
+    UnifiedAccountingCompanyinfoOutput | UnifiedAccountingCompanyinfoOutput[]
+  > {
+    return Promise.resolve({});
+  }
+}
