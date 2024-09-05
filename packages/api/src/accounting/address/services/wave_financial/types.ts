2efa9059623e69398fef870450fcf22578739e90
maishaSupritee <maisha.sup@gmail.com>
2024-09-05 11:45:37 -0700

    ✨ Add types.ts for address

---
+++ packages/api/src/accounting/address/services/wave_financial/types.ts
@@ -0,0 +1,30 @@
+export interface Province {
+  slug?: string; // Marked as deprecated in the API
+  code: string;
+  name: string;
+}
+
+export interface Country {
+  name: string;
+  currency: Currency;
+  nameWithArticle?: string; //Name of the country with the appropriate article, e.g. "the United States"
+  provinces: Province[];
+}
+
+export interface Currency {
+  symbol: string;
+  name: string;
+  plural?: string; //Plural version of currency name
+  exponent?: number; //The number of digits found to the right of the decimal place to represent the fractional part of this currency (assumes a base of 10).
+}
+
+export interface WaveFinancialAddress {
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+  province: Province;
+  country: Country;
+  postalCode: string;
+}
+export type WaveFinancialAddressInput = Partial<WaveFinancialAddress>;
+export type WaveFinancialAddressOutput = WaveFinancialAddressInput;
