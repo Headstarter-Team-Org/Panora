a11218294f3bdf64cf83d2a49107fd39e00113bf
colexeco <evan.shoemaker0@gmail.com>
2024-09-04 18:09:30 -0700

    implement accounting IContactService

--- packages/api/src/accounting/contact/services/wave_financial/index.ts
+++ packages/api/src/accounting/contact/services/wave_financial/index.ts
@@ -3,23 +3,11 @@ import { LoggerService } from '@@core/@core-services/logger/logger.service';
 import { PrismaService } from '@@core/@core-services/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { ServiceRegistry } from '../../../../@core/connections/accounting/services/registry.service';
-import { DesunifyReturnType } from '@@core/utils/types/desunify.input';
-import { OriginalContactOutput } from '@@core/utils/types/original/original.accounting';
 import { ApiResponse } from '@@core/utils/types';
 import { AccountingObject } from '../../../../../../shared/src/standardObjects';
 import { AccountingObjectInput } from '@@core/utils/types/original/original.accounting';
 import { WaveFinancialContactOutput } from './types';
-
-export interface IContactService {
-    addContact(
-      contactData: DesunifyReturnType,
-      linkedUserId: string
-    ): Promise<ApiResponse<OriginalContactOutput>>;
-  
-    syncContacts(
-      linkedUserId: string
-    ): Promise<ApiResponse<OriginalContactOutput[]>>;
-}  
+import { IContactService } from '@accounting/contact/types';
 
 @Injectable()
 export class WaveFinancial implements IContactService {
