32b5412aebebbafa24e395391082f0c3438b1147
colexeco <evan.shoemaker0@gmail.com>
2024-09-06 15:42:03 -0700

    ✨ Add constructor to contact"
    Add constructor to contact

--- packages/api/src/accounting/contact/services/wave_financial/index.ts
+++ packages/api/src/accounting/contact/services/wave_financial/index.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { ApiResponse } from '@@core/utils/types';
+import { EncryptionService } from '@@core/@core-services/encryption/encryption.service';
+import { LoggerService } from '@@core/@core-services/logger/logger.service';
+import { PrismaService } from '@@core/@core-services/prisma/prisma.service';
+import { ServiceRegistry } from '../registry.service';
 import { AccountingObjectInput } from '@@core/utils/types/original/original.accounting';
-import { WaveFinancialContactOutput } from './types';
+import { AccountingObject } from '@accounting/@lib/@types'
 import { IContactService } from '@accounting/contact/types';
 import { SyncParam } from '@@core/utils/types/interface';
 
 @Injectable()
 export class WaveFinancial implements IContactService {
+  constructor(
+    private prisma: PrismaService,
+    private logger: LoggerService,
+    private cryptoService: EncryptionService,
+    private registry: ServiceRegistry,
+  ) {
+    this.logger.setContext(
+      AccountingObject.contact.toUpperCase() + ':' + WaveFinancial.name,
+    );
+    this.registry.registerService('my3rdParty', this);
+  }
   addContact(contactData: AccountingObjectInput, linkedUserId: string) {
-    return Promise<ApiResponse<WaveFinancialContactOutput>>;
+    
+    return;
   }
 
   sync(data: SyncParam) {
-    return Promise<ApiResponse<WaveFinancialContactOutput[]>>;
+    return;s
   }
 }
