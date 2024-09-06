6b60426585beb9ad2d130e805a16a1c9bc233799
colexeco <evan.shoemaker0@gmail.com>
2024-09-05 18:04:34 -0700

    stub out contact index from accounting contact index

--- packages/api/src/accounting/contact/services/wave_financial/index.ts
+++ packages/api/src/accounting/contact/services/wave_financial/index.ts
@@ -1,55 +1,17 @@
-import { EncryptionService } from '@@core/@core-services/encryption/encryption.service';
-import { LoggerService } from '@@core/@core-services/logger/logger.service';
-import { PrismaService } from '@@core/@core-services/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
-import { ServiceRegistry } from '../../../../@core/connections/accounting/services/registry.service';
 import { ApiResponse } from '@@core/utils/types';
-import { AccountingObject } from '../../../../../../shared/src/standardObjects';
 import { AccountingObjectInput } from '@@core/utils/types/original/original.accounting';
 import { WaveFinancialContactOutput } from './types';
 import { IContactService } from '@accounting/contact/types';
+import { SyncParam } from '@@core/utils/types/interface';
 
 @Injectable()
 export class WaveFinancial implements IContactService {
-  constructor(
-        private prisma: PrismaService,
-        private logger: LoggerService,
-        private cryptoService: EncryptionService,
-        private registry: ServiceRegistry,
-    ) {
-    this.logger.setContext(
-        AccountingObject.contact.toUpperCase() + ':' + WaveFinancial.name,
-    );
-    this.registry.registerService('wave_financial', this);
-  }
-  async addContact(
-    contactData: AccountingObjectInput,
-    linkedUserId: string,
-  ): Promise<ApiResponse<WaveFinancialContactOutput>> {
-    return <ApiResponse<WaveFinancialContactOutput>>;
+  addContact(contactData: AccountingObjectInput, linkedUserId: string) {
+    return Promise<ApiResponse<WaveFinancialContactOutput>>;
   }
 
-  async syncContacts(
-    linkedUserId: string,
-  ): Promise<ApiResponse<WaveFinancialContactOutput[]>> {
-    return <ApiResponse<WaveFinancialContactOutput[]>>;
-  }
-
-  async handleCallback(
-
-  ): Promise<ApiResponse<WaveFinancialContactOutput>> {
-    return <ApiResponse<WaveFinancialContactOutput>>;
-  }
-
-  async passthrough(
-
-  ): Promise<ApiResponse<WaveFinancialContactOutput>> {
-    return <ApiResponse<WaveFinancialContactOutput>>;
-  }
-
-  async constructPassthrough(
-
-  ): Promise<ApiResponse<WaveFinancialContactOutput>> {
-    return <ApiResponse<WaveFinancialContactOutput>>;
+  sync(data: SyncParam) {
+    return Promise<ApiResponse<WaveFinancialContactOutput[]>>;
   }
 }
